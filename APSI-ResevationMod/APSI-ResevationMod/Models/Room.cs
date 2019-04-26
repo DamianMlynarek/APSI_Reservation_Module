4b0245bc6e5dacaafd1e97230a50b4c2dcb50d28
Damian Mlynarek <damnian24@gmail.com>
2019-04-26 11:03:39 +0200

    New models

---
+++ APSI-ResevationMod/APSI-ResevationMod/Models/Room.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APSI_ResevationMod.Models
+{
+    public class Room
+    {
+        public string RoomNumber { get; set; }
+
+    }
+}
